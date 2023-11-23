cef574a43990ec2040ff3b5df14f94ddc2a8098f
Anthony Viano <anthony.viano@etu.unice.fr>
2023-11-23 12:42:13 +0100

    url

--- project/urls.py
+++ project/urls.py
@@ -1,7 +1,14 @@
+from rest_framework import routers
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+router = routers.SimpleRouter()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='obtain_tokens'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='refresh_token'),
+    path('api/', include(router.urls)),
 ]
